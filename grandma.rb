a819590068ced242923707a71b16c407711d0a28
YUKIKO IKEDA <applelele@gmail.com>
2020-03-05 00:19:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = ""
+  elsif phrase = "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
